acd7866df42916e9594faf08f6ffd52fb7155f67
leonardo-borges <leonardo-borges@hotmail.com>
2018-06-13 20:34:59 -0400

    Update indexnovaok

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class Test(unittest.TestCase):
         result = self.app.get('/') 
 
         # verifica o retorno do conteudo da pagina
-        self.assertRegex(result.data.decode(), "DevOps Leonardo Top")
+        self.assertRegex(result.data.decode(), "Aplicação DevOps Leonardo")
 
 
 if __name__ == "__main__":
